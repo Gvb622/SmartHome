329376db1c03db6859f83282cbeb42275cdcaae4
libopz <razerkiritoz@gmail.com>
2016-09-10 21:53:13 +0700

    graph
    line graph

--- app/src/main/java/com/smart/smarthome/MonthlyReportActivity.java
+++ app/src/main/java/com/smart/smarthome/MonthlyReportActivity.java
@@ -1,6 +1,7 @@
 package com.smart.smarthome;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,62 +14,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import lecho.lib.hellocharts.gesture.ZoomType;
-import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
-import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
-import lecho.lib.hellocharts.model.Axis;
-import lecho.lib.hellocharts.model.AxisValue;
-import lecho.lib.hellocharts.model.Column;
-import lecho.lib.hellocharts.model.ColumnChartData;
-import lecho.lib.hellocharts.model.Line;
+import im.dacer.androidcharts.LineView;
 import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.model.SubcolumnValue;
-import lecho.lib.hellocharts.model.ValueShape;
-import lecho.lib.hellocharts.model.Viewport;
-import lecho.lib.hellocharts.util.ChartUtils;
-import lecho.lib.hellocharts.view.Chart;
-import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.view.LineChartView;
 
 public class MonthlyReportActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    private LineChartView chart;
-    private LineChartData data;
-    private int numberOfLines = 1;
-    private int maxNumberOfLines = 4;
-    private int numberOfPoints = 12;
-
-    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
-
-    private boolean hasAxes = true;
-    private boolean hasAxesNames = true;
-    private boolean hasLines = true;
-    private boolean hasPoints = true;
-    private ValueShape shape = ValueShape.CIRCLE;
-    private boolean isFilled = false;
-    private boolean hasLabels = false;
-    private boolean isCubic = false;
-    private boolean hasLabelForSelected = false;
-    private boolean pointsHaveDifferentColor;
-
     public final static String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
             "Sep", "Oct", "Nov", "Dec",};
 
 
 
+    int randomint = 9;
 
 
     @Override
@@ -79,18 +53,27 @@ public class MonthlyReportActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-        chart = (LineChartView) findViewById(R.id.chart);
-        chart.setOnValueTouchListener(new ValueTouchListener());
+        final LineView lineView = (LineView)findViewById(R.id.line_view);
 
-        // Generate some random values.
-        generateValues();
+        //must*
+        ArrayList<String> test = new ArrayList<String>();
+        for (int i=0; i<randomint; i++){
+            test.add("sdfsdf");
+        }
+        lineView.setBottomTextList(test);
+        lineView.setDrawDotLine(true);
+        lineView.setShowPopup(LineView.SHOW_POPUPS_All);
 
-        generateData();
+        Button lineButton = (Button)findViewById(R.id.line_button);
+        lineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                randomSet(lineView);
 
-        // Disable viewport recalculations, see toggleCubic() method for more info.
-        chart.setViewportCalculationEnabled(false);
+            }
+        });
 
-        resetViewport();
+        randomSet(lineView);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -103,89 +86,31 @@ public class MonthlyReportActivity extends AppCompatActivity
     }
 
 
-    private void resetViewport() {
-        // Reset viewport height range to (0,100)
-        final Viewport v = new Viewport(chart.getMaximumViewport());
-        v.bottom = 0;
-        v.top = 100;
-        v.left = 0;
-        v.right = numberOfPoints - 1;
-        chart.setMaximumViewport(v);
-        chart.setCurrentViewport(v);
-    }
-
-
-
-
-    private void generateValues() {
-        for (int i = 0; i < maxNumberOfLines; ++i) {
-            for (int j = 0; j < numberOfPoints; ++j) {
-                randomNumbersTab[i][j] = (float) Math.random() * 100f;
-            }
-        }
-    }
-
-
-    private void generateData() {
-
-        List<Line> lines = new ArrayList<Line>();
-        for (int i = 0; i < numberOfLines; ++i) {
-
-            List<PointValue> values = new ArrayList<PointValue>();
-            for (int j = 0; j < numberOfPoints; ++j) {
-                values.add(new PointValue(j, randomNumbersTab[i][j]));
-            }
-
-            Line line = new Line(values);
-            line.setColor(ChartUtils.COLORS[i]);
-            line.setShape(shape);
-            line.setCubic(isCubic);
-            line.setFilled(isFilled);
-            line.setHasLabels(hasLabels);
-            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
-            line.setHasLines(hasLines);
-            line.setHasPoints(hasPoints);
-            if (pointsHaveDifferentColor){
-                line.setPointColor(ChartUtils.COLORS[(i + 1) % ChartUtils.COLORS.length]);
-            }
-            lines.add(line);
+    private void randomSet(LineView lineView){
+        ArrayList<Integer> dataList = new ArrayList<Integer>();
+        int random = (int)(Math.random()*9+1);
+        for (int i=0; i<randomint; i++){
+            dataList.add((int)(Math.random()*random));
         }
 
-        data = new LineChartData(lines);
-
-        if (hasAxes) {
-            Axis axisX = new Axis();
-            Axis axisY = new Axis().setHasLines(true);
-            if (hasAxesNames) {
-                axisX.setName("Axis X");
-                axisY.setName("Axis Y");
-            }
-            data.setAxisXBottom(axisX);
-            data.setAxisYLeft(axisY);
-        } else {
-            data.setAxisXBottom(null);
-            data.setAxisYLeft(null);
+        ArrayList<Integer> dataList2 = new ArrayList<Integer>();
+        random = (int)(Math.random()*9+1);
+        for (int i=0; i<randomint; i++){
+            dataList2.add((int)(Math.random()*random));
         }
 
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        chart.setLineChartData(data);
-
-    }
-
-
-    private class ValueTouchListener implements LineChartOnValueSelectListener {
-
-        @Override
-        public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
-            //Toast.makeText(getActivity(), "Selected: " + value, Toast.LENGTH_SHORT).show();
+        ArrayList<Integer> dataList3 = new ArrayList<Integer>();
+        random = (int)(Math.random()*9+1);
+        for (int i=0; i<randomint; i++){
+            dataList3.add((int)(Math.random()*random));
         }
 
-        @Override
-        public void onValueDeselected() {
-            // TODO Auto-generated method stub
+        ArrayList<ArrayList<Integer>> dataLists = new ArrayList<ArrayList<Integer>>();
+        dataLists.add(dataList);
+        dataLists.add(dataList2);
+//        dataLists.add(dataList3);
 
-        }
-
+        lineView.setDataList(dataLists);
     }
 
     @Override
@@ -247,4 +172,5 @@ public class MonthlyReportActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
